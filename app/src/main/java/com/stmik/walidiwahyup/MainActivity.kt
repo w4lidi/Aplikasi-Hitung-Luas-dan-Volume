b8d241f900210ba8e1dfc003e1f84f8bcd298949
tedi0609 <tedysahputra692@gmail.com>
2023-11-03 19:26:21 +0800

    Change view to ViewBinding

--- app/src/main/java/com/stmik/walidiwahyup/MainActivity.kt
+++ app/src/main/java/com/stmik/walidiwahyup/MainActivity.kt
@@ -3,65 +3,72 @@ package com.stmik.walidiwahyup
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 
+import androidx.core.content.ContextCompat
+import com.stmik.walidiwahyup.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var lebar : EditText
-    private lateinit var panjang : EditText
-    private  lateinit var tombol : Button
-    private  lateinit var hasil : TextView
-    private lateinit var tinggi : EditText
+//    private lateinit var lebar : EditText
+//    private lateinit var panjang : EditText
+//    private  lateinit var tombol : Button
+//    private  lateinit var hasil : TextView
+//    private lateinit var tinggi : EditText
+
+    private lateinit var binding : ActivityMainBinding
 
     @SuppressLint("Range")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        panjang = findViewById(R.id.editTextPanjang)
-        lebar = findViewById(R.id.editTextLebar)
-        tinggi = findViewById(R.id.editTextTinggi)
+//        panjang = findViewById(R.id.editTextPanjang)
+//        lebar = findViewById(R.id.editTextLebar)
+//        tinggi = findViewById(R.id.editTextTinggi)
+//
+//        tombol = findViewById(R.id.button)
+//        hasil = findViewById(R.id.editTextHasil)
 
-        tombol = findViewById(R.id.button)
-        hasil = findViewById(R.id.editTextHasil)
 
 
-
-        tombol.setOnClickListener {
-            val nilaitinggi = tinggi.text.toString()
-            val nilaipanjang = panjang.text.toString()
-            val nilailebar = lebar.text.toString()
+        binding.button.setOnClickListener {
+            val nilaitinggi = binding.editTextTinggi.text.toString()
+            val nilaipanjang = binding.editTextPanjang.text.toString()
+            val nilailebar = binding.editTextLebar.text.toString()
             val persegi = " m2"
             val kubik = " m3"
             if (nilaipanjang.isEmpty() || nilailebar.isEmpty()){
                 if (nilaipanjang.isEmpty() && nilailebar.isNotEmpty()){
                     //val msg = "Masukkan Nilai Panjang"
-                    panjang.setHintTextColor(ContextCompat.getColor(this, R.color.error))
+                    binding.editTextPanjang.setHintTextColor(ContextCompat.getColor(this, R.color.error))
                 }else if (nilailebar.isEmpty() && nilaipanjang.isNotEmpty()){
-                    lebar.setHintTextColor(ContextCompat.getColor(this, R.color.error))
-            }else{
-                    panjang.setHintTextColor(ContextCompat.getColor(this, R.color.error))
-                    lebar.setHintTextColor(ContextCompat.getColor(this, R.color.error))
+                    binding.editTextLebar.setHintTextColor(ContextCompat.getColor(this, R.color.error))
+                }else if (nilailebar.isEmpty() && nilaipanjang.isEmpty()){
+                    binding.editTextPanjang.setHintTextColor(ContextCompat.getColor(this, R.color.error))
+                    binding.editTextLebar.setHintTextColor(ContextCompat.getColor(this, R.color.error))
 
                 }
             }else if (nilaipanjang.isNotEmpty() && nilailebar.isNotEmpty() && nilaitinggi.isEmpty()){
-                panjang.setHintTextColor(ContextCompat.getColor(this, R.color.hint))
-                lebar.setHintTextColor(ContextCompat.getColor(this, R.color.hint))
+                binding.editTextPanjang.setHintTextColor(ContextCompat.getColor(this, R.color.hint))
+                binding.editTextLebar.setHintTextColor(ContextCompat.getColor(this, R.color.hint))
 
                 val nilaihasil = nilaipanjang.toInt() * nilailebar.toInt()
                 val hasilstring =  nilaihasil.toString() + persegi
-                hasil.text = hasilstring
+
+//                val hasilstring = "w"
+
+                binding.editTextHasil.text = hasilstring
             }else if (nilaipanjang.isNotEmpty() && nilailebar.isNotEmpty() && nilaitinggi.isNotEmpty()){
-                panjang.setHintTextColor(ContextCompat.getColor(this, R.color.hint))
-                lebar.setHintTextColor(ContextCompat.getColor(this, R.color.hint))
+                binding.editTextPanjang.setHintTextColor(ContextCompat.getColor(this, R.color.hint))
+                binding.editTextLebar.setHintTextColor(ContextCompat.getColor(this, R.color.hint))
 
                 val nilaihasil = nilaipanjang.toInt() * nilailebar.toInt() * nilaitinggi.toInt()
                 val hasilstring = nilaihasil.toString() + kubik
-                hasil.text = hasilstring
+
+//                val hasilstring = nilaipanjang.toString()
+
+                binding.editTextHasil.text = hasilstring
             }
         }
 
