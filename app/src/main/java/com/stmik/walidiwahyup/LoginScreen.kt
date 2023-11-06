bfcfee885ddb8690b383421a2e8c1298faf65196
w4lidi <walidiwahyupratama@gmail.com>
2023-11-06 21:08:16 +0800

    Add login system

--- app/src/main/java/com/stmik/walidiwahyup/LoginScreen.kt
+++ app/src/main/java/com/stmik/walidiwahyup/LoginScreen.kt
@@ -2,10 +2,47 @@ package com.stmik.walidiwahyup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.stmik.walidiwahyup.databinding.ActivityLoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
+
+    private lateinit var login : ActivityLoginScreenBinding
+
+    val username = "w4lidi"
+    val pass = "walidi"
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
+        login = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(login.root)
+
+        login.buttonLogin.setOnClickListener {
+            val inputUserName = login.editTextUsername.text.toString()
+            val inputPassword = login.editTextPassword.text.toString()
+            val log = login.textLog
+            if (inputPassword.isEmpty() && inputUserName.isEmpty()){
+                log.setTextColor(ContextCompat.getColor(this, R.color.error))
+                log.text = "silahkan isi Username & Password"
+            }
+            else if (inputPassword.isEmpty() && inputUserName.isNotEmpty()){
+                log.setTextColor(ContextCompat.getColor(this, R.color.error))
+                log.text = "Passwor is missing"
+            }else if (inputPassword.isNotEmpty() && inputUserName.isEmpty()){
+                log.setTextColor(ContextCompat.getColor(this, R.color.error))
+                log.text = "Username is missing"
+            }else{
+                log.text = ""
+                if ((inputUserName == username) && (inputPassword == pass)){
+                    log.setTextColor(ContextCompat.getColor(this, R.color.success))
+                    log.text = "Selamat ... \n Anda berhasil login."
+                }else{
+                    log.setTextColor(ContextCompat.getColor(this, R.color.error))
+                    log.text = "Try again... \n Login Failed."
+                }
+            }
+        }
+
+
     }
 }
